74dcfa4916be3846c51c374439fc80851072fe11
Helton Mori <heltonmori@gmail.com>
2024-11-09 17:39:13 -0300

    Replacing vue-cli template files

---
+++ src/views/SignUpView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>
+    <h1>Start your flashcard study</h1>
+    <h2>Sign up to DeckMaster</h2>
+    <input type="text" placeholder="Your name" />
+    <input type="text" placeholder="Email address" />
+    <input type="password" />
+    <button type="submit">Sign up</button>
+  </main>
+</template>
