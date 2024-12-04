801a5a497051938919acaee7f9e38b029404d1a1
Helton Mori <heltonmori@gmail.com>
2024-12-04 14:41:22 -0300

    Preparação para build

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,14 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [vue(), vueDevTools()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
-  },
+    base: mode === 'production' ? '/flashcard-app-front/' : '',
+  }
 })
